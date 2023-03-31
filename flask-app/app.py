4611b82881fb0caeb97e8a9e1f127f84036f4ee2
Sulthan <sbun0004@student.monash.edu>
2023-03-31 08:37:43 +0800

    added mysql connection to the database

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,10 +1,31 @@
 from flask import Flask
+import mysql.connector
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
+mydb = mysql.connector.connect(
+  host='rental-data.cckjcfdnzspp.ap-southeast-2.rds.amazonaws.com',
+  user='asur0015',
+  password='finalsemproject'
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("select * from rental_data.rental limit 10;")
+
+myresult = mycursor.fetchall()
+
 @app.route('/')
 def home():
-    return 'Hello world!'
+
+    total = []
+
+    for item in myresult:
+        total.append(str(item[0])+" "+str(item[1])+" "+str(item[2])+" "+str(item[3])+" "+str(item[4])+" "+str(item[5])+" "+str(item[6]))
+    
+    total = ''.join(total)
+
+    return total
 
 if __name__ == '__main__':
     app.run()
