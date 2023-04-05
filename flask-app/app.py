524ec39e7a4764fb2f19a80059b56fd748a69d7c
Sulthan <sbun0004@student.monash.edu>
2023-04-06 06:23:10 +0800

    added password

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, request
 import mysql.connector
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
+
+app.secret_key = 'mysecret'
 
 mydb = mysql.connector.connect(
   host='rental-data.cckjcfdnzspp.ap-southeast-2.rds.amazonaws.com',
@@ -15,19 +17,38 @@ myresult = mycursor.fetchall()
 
 @app.route('/')
 def home():
+    if 'logged_in' not in session:
+        return redirect(url_for('login'))
     return render_template('home.html')
-
-@app.route('/suburb')
-def suburb():
-    return render_template('suburb.html')
 
 @app.route('/suburb/forecast')
 def suburb_forecast():
+    if 'logged_in' not in session:
+        return redirect(url_for('login'))
     return render_template('suburb_forecast.html')
 
 @app.route('/suburb/historicaldata')
 def suburb_historicaldata():
+    if 'logged_in' not in session:
+        return redirect(url_for('login'))
     return render_template('suburb_historicaldata.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Check the login credentials
+        if request.form['username'] == 'notsoeasytoguess' and request.form['password'] == 'notsoeasytoguess':
+            # Save the login status in the session
+            session['logged_in'] = True
+            return redirect(url_for('home'))
+    
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+    '''
 
 if __name__ == '__main__':
     app.run()
