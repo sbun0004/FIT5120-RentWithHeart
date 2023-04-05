24e4b2b7a0d277c75f9c395a34e579975fe142af
Sulthan <sbun0004@student.monash.edu>
2023-04-06 02:13:08 +0800

    added forecast and historicaldata html

--- flask-app/app.py
+++ flask-app/app.py
@@ -13,22 +13,21 @@ mycursor = mydb.cursor()
 mycursor.execute("select * from rental_data.rental limit 10;")
 myresult = mycursor.fetchall()
 
-
-# @app.route('/')
-# def homeTest():
-
-#     total = []
-
-#     for item in myresult:
-#         total.append(str(item[0])+" "+str(item[1])+" "+str(item[2])+" "+str(item[3])+" "+str(item[4])+" "+str(item[5])+" "+str(item[6]))
-    
-#     total = ''.join(total)
-
-#     return render_template('home.html')
-
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/suburb')
+def suburb():
+    return render_template('suburb.html')
+
+@app.route('/suburb/forecast')
+def suburb_forecast():
+    return render_template('suburb_forecast.html')
+
+@app.route('/suburb/historicaldata')
+def suburb_historicaldata():
+    return render_template('suburb_historicaldata.html')
 
 if __name__ == '__main__':
     app.run()
