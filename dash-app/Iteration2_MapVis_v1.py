d50e125c5d15c938bf2f0441f9ac8d9c990cb472
Weiheng Ho <whoo0032@student.monash.edu>
2023-04-26 22:26:18 +1000

    Changed path of files

--- dash-app/Iteration2_MapVis_v1.py
+++ dash-app/Iteration2_MapVis_v1.py
@@ -4,8 +4,8 @@ import geopandas as gpd
 import plotly.express as px
 import dash_bootstrap_components as dbc
 
-forecast_path = 'df_forecast_cleaned.csv'
-localities_path= 'vic_localities_cleaned/vic_localities_cleaned.shp'
+forecast_path = 'https://raw.githubusercontent.com/sbun0004/FIT5120-RentWithHeart/main/dash-app/df_forecast_cleaned.csv'
+localities_path= 'https://github.com/sbun0004/FIT5120-RentWithHeart/blob/main/dash-app/vic_localities_cleaned/vic_localities_cleaned.shp?raw=true'
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 server = app.server
