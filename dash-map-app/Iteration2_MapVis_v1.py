2eb39b050c37cb96cef6335d883a2163d5e75319
Weiheng Ho <whoo0032@student.monash.edu>
2023-04-27 13:16:23 +1000

    updated paths of forecast and localities

--- dash-map-app/Iteration2_MapVis_v1.py
+++ dash-map-app/Iteration2_MapVis_v1.py
@@ -4,8 +4,8 @@ import geopandas as gpd
 import plotly.express as px
 import dash_bootstrap_components as dbc
 
-forecast_path = 'https://raw.githubusercontent.com/sbun0004/FIT5120-RentWithHeart/main/dash-app/df_forecast_cleaned.csv'
-localities_path= 'https://github.com/sbun0004/FIT5120-RentWithHeart/blob/main/dash-app/vic_localities_cleaned/vic_localities_cleaned.shp?raw=true'
+forecast_path = 'https://raw.githubusercontent.com/sbun0004/FIT5120-RentWithHeart/main/dash-map-app/df_forecast_cleaned.csv'
+localities_path= 'https://raw.githubusercontent.com/sbun0004/FIT5120-RentWithHeart/main/dash-map-app/vic_localities_cleaned.geojson'
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 server = app.server
